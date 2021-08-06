aef3fcc957ad97808b7882b1c73bbd4e6faf904c
yamuna <yam@gmail.com>
2021-08-06 22:09:56 +0000

    five

--- simple.java
+++ simple.java
@@ -2,6 +2,6 @@ public class HelloWorld {
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-	System.out.println ("Heldo Friend, How are you?!");
+	System.out.println ("Hello Friend, How are you?!");
     }
 }
